4161e58ececcd750ab668f627e96e407f9b1cb6a
Spencer <spencerrca@gmail.com>
2019-06-10 18:06:23 -0400

    Fixed bug w/ play/pause functionality

--- src/components/MainDisplay.js
+++ src/components/MainDisplay.js
@@ -1,98 +1,104 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import YouTube from 'react-youtube';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import YouTube from "react-youtube";
 
 class MainDisplay extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.mainDisplay = React.createRef();
+    this.musicPlayer = React.createRef();
   }
-  playPause = (e) => {
+  componentDidMount() {
+    this.mainDisplay.current.addEventListener("click", e => {
+      this.playPause();
+    });
+  }
+  playPause = async e => {
+    const { internalPlayer } = this.musicPlayer.current;
+    const playerState = await internalPlayer.getPlayerState();
+
     // if the player state is 1 - video is playing, run pause function on key press
-    if (e.target.j.playerState === 1) {
-      e.target.pauseVideo()
-    } else if (e.target.j.playerState === 2) {
+    if (playerState === 1) {
+      internalPlayer.pauseVideo();
+    } else if (playerState === 2) {
       // if player state is 2 - video is paused, run play function on key press
-      e.target.playVideo()
+      internalPlayer.playVideo();
+    } else {
+      return;
     }
-    
-  }
-  changeHandler = (e) => {
-    document.addEventListener('keypress', (key) => {
-      if (key.code === "Space" || key.code === "KeyK"){
-        this.playPause(e)
+  };
+  changeHandler = e => {
+    document.addEventListener("keypress", key => {
+      if (key.code === "Space" || key.code === "KeyK") {
+        this.playPause();
       }
-    })
-    
-    this.mainDisplay.current.addEventListener('click', () => {
-      this.playPause(e)
-    })
-    
+    });
+
     // unmute video, to ensure consistency
-    e.target.unMute()
-  }
-  getActiveStationDetails = (activeStation) => {
-    const stationList = this.props.stationList.items
-    const stationDetails = stationList.filter((station) => station.id.videoId === activeStation)
-    const pageTitle = document.getElementById('pageTitle')
+    e.target.unMute();
+  };
+  getActiveStationDetails = activeStation => {
+    const stationList = this.props.stationList.items;
+    const stationDetails = stationList.filter(
+      station => station.id.videoId === activeStation
+    );
+    const pageTitle = document.getElementById("pageTitle");
     let details = {};
-    if(stationDetails[0]){
-      pageTitle.innerHTML = `That Radio - ${stationDetails[0].snippet.channelTitle}`
-      details = stationDetails[0].snippet
+    if (stationDetails[0]) {
+      pageTitle.innerHTML = `That Radio - ${
+        stationDetails[0].snippet.channelTitle
+      }`;
+      details = stationDetails[0].snippet;
     }
-    return details
-  }
+    return details;
+  };
   render() {
-    const {activeStation, activeGif} = this.props
-    
+    const { activeStation, activeGif } = this.props;
+
     return (
-      <section ref={this.mainDisplay}>
-      {
-        activeStation.item ?
+      <section ref={this.mainDisplay} className="mainDisplay">
+        {activeStation.item ? (
           <YouTube
             videoId={activeStation.item}
             id="videoFrame"
             className="videoFrame visuallyhidden"
+            ref={this.musicPlayer}
             onReady={this.changeHandler}
-            opts={
-              {
-                height: '390',
-                width: '640',
-                playerVars: {
-                  autoplay: 1
-                }
+            opts={{
+              height: "390",
+              width: "640",
+              playerVars: {
+                autoplay: 1
               }
-            }
+            }}
           />
-        : null
-      }
-      {
-        activeStation.item ?
-          <h3 className="activeStation">{this.getActiveStationDetails(activeStation.item)["channelTitle"]}</h3>
-        : null
-      }
-      {
-        activeGif.item ? 
+        ) : null}
+        {activeStation.item ? (
+          <h3 className="activeStation">
+            {this.getActiveStationDetails(activeStation.item)["channelTitle"]}
+          </h3>
+        ) : null}
+        {activeGif.item ? (
           <video
-          className="gifOverlay"
-          id="gifOverlay"
-          aria-labelledby="Active Gif Background"
-          src={activeGif.item}
-          type="video/mp4"
-          autoPlay
-          loop>
+            className="gifOverlay"
+            id="gifOverlay"
+            aria-labelledby="Active Gif Background"
+            src={activeGif.item}
+            type="video/mp4"
+            autoPlay
+            loop
+          >
             {/* <source src={this.props.activeGif.activeGif} type="video/mp4" /> */}
             Your browser does not support HTML5 video (╥﹏╥).
           </video>
-        : null
-      }
+        ) : null}
       </section>
-    )
+    );
   }
 }
 const mapStateToProps = state => ({
   activeStation: state.activeStation,
   stationList: state.stationList,
   activeGif: state.activeGif
-})
+});
 export default connect(mapStateToProps)(MainDisplay);
