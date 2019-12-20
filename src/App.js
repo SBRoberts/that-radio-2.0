c83d04deb4c177f090f80ed5b90f08151072b987
Spencer R <sroberts@theworkinggroup.ca>
2019-12-20 11:33:33 -0500

    fixed minor layout issues

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import {Provider} from 'react-redux';
-import './App.css';
-import logo from './assets/boombox.svg'
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import "./App.css";
+import logo from "./assets/boombox.svg";
 
-import store from './store';
+import store from "./store";
 
-import ControlPanel from './components/ControlPanel'
-import MainDisplay from './components/MainDisplay'
-import GifPanel from './components/GifPanel'
+import ControlPanel from "./components/ControlPanel";
+import MainDisplay from "./components/MainDisplay";
+import GifPanel from "./components/GifPanel";
 
 class App extends Component {
   render() {
