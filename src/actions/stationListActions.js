30bba6edd9797616e902209b0905a51e748e43ba
Spencer R <sroberts@theworkinggroup.ca>
2020-03-29 23:08:28 -0400

    actually fixed and secured api key, discarded useless old api key

--- src/actions/stationListActions.js
+++ src/actions/stationListActions.js
@@ -1,33 +1,31 @@
-import { FETCH_STATIONLIST } from './types';
-import axios from 'axios';
+import { FETCH_STATIONLIST } from "./types";
+import axios from "axios";
 
-export const fetchStationList = (queryTerm) => dispatch =>{
-  
+export const fetchStationList = queryTerm => dispatch => {
   const getStations = () => {
-    const apiKey = 'AIzaSyBwCL-n9i1xIFVtVYbHvzNHjtG3QQLWnMA'
-    const baseUrl = 'https://www.googleapis.com/youtube/v3/'
+    const apiKey = process.env.REACT_APP_GOOGLE_DATA_KEY;
+    const baseUrl = "https://www.googleapis.com/youtube/v3/";
     axios({
-      method: 'GET',
+      method: "GET",
       url: `${baseUrl}search?part=snippet`,
-      dataResponse: 'json',
+      dataResponse: "json",
       params: {
         q: queryTerm,
-        type: 'video',
-        eventType: 'live',
-        videoCategoryId: '10',
-        videoEmbeddable: 'true',
-        maxResults: '5',
-        key: apiKey,
+        type: "video",
+        eventType: "live",
+        videoCategoryId: "10",
+        videoEmbeddable: "true",
+        maxResults: "5",
+        key: apiKey
       }
-    })
-    .then(({ data }) => {
-      const stationList = data.items || 'undefined'
+    }).then(({ data }) => {
+      const stationList = data.items || "undefined";
       dispatch({
         type: FETCH_STATIONLIST,
         payload: stationList
-      })
-      return stationList
-    })
-  }
-  getStations()
-}
+      });
+      return stationList;
+    });
+  };
+  getStations();
+};
